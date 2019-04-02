38f4b6894c8edcaf2dee122b93fa569c109e9b44
patrick <patrick.lin@genesys.com>
2019-04-02 17:12:28 -0400

    removed ping

--- bot.js
+++ bot.js
@@ -8,11 +8,7 @@ var general_channel_id = config.getGeneralChannel()
 
 
 client.on('ready', () => {
-    // List servers the bot is connected to
-    var http = require("http");
-    setInterval(function() {
-        http.get("https://evance-discord-bot.herokuapp.com/");
-    }, 300000); // every 5 minutes (300000)
+
 })
 client.on('message', (receivedMessage) => {
     // Prevent bot from responding to its own messages
